d4532e00a420215ba1a6380045f6a2700a70f5f5
Konstantin <grahovsky@gmail.com>
2018-09-12 21:01:07 +0300

    20180912_4

--- FirstIOSApps/ViewController.swift
+++ FirstIOSApps/ViewController.swift
@@ -10,16 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func pressButton(_ sender: UIButton) {
+    
+        label.text = "First App!"
+    
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
