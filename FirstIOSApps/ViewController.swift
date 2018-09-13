e5da60df0ec6d4f6430a15806b39395777718e87
Konstantin <grahovsky@gmail.com>
2018-09-13 09:53:40 +0300

    20180913_2

--- FirstIOSApps/ViewController.swift
+++ FirstIOSApps/ViewController.swift
@@ -10,13 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var dateTF: UITextField!
+    @IBOutlet weak var dayTF: UITextField!
     @IBOutlet weak var monthTF: UITextField!
     @IBOutlet weak var yearTF: UITextField!
     
-    @IBOutlet weak var resultLaberl: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
     
-    @IBAction func findDay(){
+    @IBAction func findWeekDay(){
+        let calendar = Calendar.current
+        
+        var dateComponents = DateComponents()
+        guard let day = dayTF.text, let month = monthTF.text, let year = yearTF.text else {return}
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year = Int(year)
+        
+        guard let date = calendar.date(from: dateComponents) else {return}
+        
+        let dateFormatter = DateFormatter()
+        //dateFormatter.locale = Locale(identifier: "ru_Ru")
+        dateFormatter.dateFormat = "EEEE"
+        
+        let weekday = dateFormatter.string(from: date)
+        
+        resultLabel.text = weekday.capitalized
         
     }
     
