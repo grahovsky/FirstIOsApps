2ef661965c0092f49a1cca07f87f0ace13edce40
Konstantin <grahovsky@gmail.com>
2018-09-13 16:40:16 +0300

    20180913_4

--- FirstIOSApps/ViewController.swift
+++ FirstIOSApps/ViewController.swift
@@ -10,16 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.maximumValue = 100
+            slider.minimumValue = 0
+            slider.value = 0
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider){
+        let temperatureCelsius = Int(round(sender.value))
+        celsiusLabel.text = "\(temperatureCelsius)ºC"
+        let temperatureFahrenheit = Int(round((sender.value * 9 / 5) + 32))
+        fahrenheitLabel.text = "\(temperatureFahrenheit)ºF"
     }
-
-
+    
+    
+    
 }
 
